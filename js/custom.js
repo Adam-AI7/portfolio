75d8c2635df88fec2cbd461a789a9abc458ff227
eitadam <adam@eitworks.com>
2024-06-07 17:19:31 +0530

    new changes update mail sending

--- js/custom.js
+++ js/custom.js
@@ -9,11 +9,42 @@ $(document).ready(function() {
 				'navigation': true,
 				'navigationPosition': 'right',
 			});
-		});
+			$('#send').on('click', function() {
+				let nameInput=$('#name').val();
+				let mail=$('#email').val();
+				let subject=$('#subject').val();
+				let message=$('#message').val();
+				console.log(message,nameInput,mail,subject);
+				let mailVerify = /^\S+@\S+\.\S+$/.test(mail);
+				if(!mailVerify || mail==''){
+					alert('Mail not Valid')
+				}
+				else if(nameInput==''){
+					alert('Name Input not Valid')
+				}else if(subject==''){
+					alert('Subject Input not Valid')
+				}else if(message==''){
+					alert('Message Input not Valid')
+				}
+				Email.send({
+					Host : "smtp.elasticemail.com",
+					Username : "adam.dev.ai7@gmail.com",
+					Password : "C78CAA2526F818EB407F780F40572F41DF60",
+					To : 'adam.dev.ai7@gmail.com',
+					From : "adam.dev.ai7@gmail.com",
+					Subject : sub||'test txt',
+					Body :mes||'working'
+				}).then(
+				  message => alert(message)
+				);
+			});
+		}); 
 
 // wow
 $(function()
 {
     new WOW().init();
     $(".rotate").textrotator();
-})
+});
+
+// $('send').
